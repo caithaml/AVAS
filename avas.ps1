28485f69a105975f4a295e031047ffa9343be18e
lukaskarabec <lukaskarabec@gmail.com>
2017-08-30 13:44:56 +0200

    úprava
    přidáno otevírání a automatické načítání xml souboru s výstupem z ICWSC, implementace GUI do powershellu

--- avas.ps1
+++ avas.ps1
@@ -3,7 +3,18 @@
 #==============================================================================================
 [void][System.Reflection.Assembly]::LoadWithPartialName('presentationframework')
 [xml]$XAML = @'
+<Window x:Class="AVAS_GUI.MainWindow"
+xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
+xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
+xmlns:local="clr-namespace:AVAS_GUI"
+mc:Ignorable="d"
+Title="MainWindow" Height="350" Width="525">
+<Grid>
 
+</Grid>
+</Window>
 '@
 #Read XAML
 $reader=(New-Object System.Xml.XmlNodeReader $xaml) 
@@ -20,6 +31,24 @@ $xaml.SelectNodes("//*[@Name]") | %{Set-Variable -Name ($_.Name) -Value $Form.Fi
 #===========================================================================
 $btnExit.Add_Click({$form.Close()}) #tlacitko konec
 
+#####################################################################################
+# Skripty a funkcionality - začátek
+#####################################################################################
+$data = Import-Clixml soubor.xml
+Function Get-FileName($initialDirectory)
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+    
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $initialDirectory
+    $OpenFileDialog.filter = "XML (*.xml)| *.xml"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    $OpenFileDialog.filename
+}
+
+#####################################################################################
+# Skripty a funkcionality - konec
+#####################################################################################
 
 
 #===========================================================================
