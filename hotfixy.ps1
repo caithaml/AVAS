8766ccd060c6cfaa04443887e1f36f348adf5f5d
lukaskarabec <lukaskarabec@gmail.com>
2017-09-11 11:08:26 +0200

    11092017-1108

--- hotfixy.ps1
+++ hotfixy.ps1
@@ -1,3 +1,4 @@
+Start-Transcript -Path "./transcript$(get-date -f yyyy-MM-dd-hh-mm-ss).txt"
 $def= Get-Content C:\SICZ\hash_mica.json | ConvertFrom-Json
 $default=$def.hotfixes
 $inst=Get-Content C:\SICZ\hash_luka.json | ConvertFrom-Json
