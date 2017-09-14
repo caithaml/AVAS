93fa3a1424fab79518f115b6fab804024425ecc7
lukaskarabec <lukaskarabec@gmail.com>
2017-09-14 15:27:21 +0200

    14092017-1527

---
+++ AVAS_LuKA/avas_LuKa.ps1
@@ -0,0 +1,25 @@
+Start-Transcript -Path "./transcript$(get-date -f yyyy-MM-dd-hh-mm-ss).txt"
+#Nacteni JSON souboru s exportovanymi informacemi ze zkusebniho rozhrani
+Write-Host -Object "$(Get-Date) - Nacitani json konfiguracniho souboru"
+$json=gc d:\SICZ\hash_mica.json | ConvertFrom-Json
+
+Write-Host -Object "$(Get-Date) - Dokonce nacitani json konfiguracniho souboru"
+
+
+Write-Host -Object "$(Get-Date) - zjisteni zda je uzivatel admin"
+#overeni ze je uzivatel administrator
+Write-Verbose "Kontroluji admin prava"
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+    [Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning "Ujistete se ze spoustite AVAS jako administrator! Provedte spusteni jako administrator nebo nemusi vse fungovat korektne!!"
+}
+
+Write-Host -Object "$(Get-Date) - ukonceno zjistovani zda je uzivatel admin"
+
+Write-Host -Object "$(Get-Date) - uzivatel je admin pokracuje dalsi spusteni skriptu"
+Write-Host -Object "$(Get-Date) - Nacitam GUI"
+
+
+#==============================================================================================
+# GUI
+#==============================================================================================
