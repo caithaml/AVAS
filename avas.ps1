706e93c4c62ef779cc4fd1e94a765668d891dc72
lukaskarabec <lukaskarabec@gmail.com>
2017-09-14 14:57:30 +0200

    14092017-1457

--- avas.ps1
+++ avas.ps1
@@ -1,7 +1,7 @@
 Start-Transcript -Path "./transcript$(get-date -f yyyy-MM-dd-hh-mm-ss).txt"
 #Nacteni JSON souboru s exportovanymi informacemi ze zkusebniho rozhrani
 Write-Host -Object "$(Get-Date) - Nacitani json konfiguracniho souboru"
-$json=gc C:\SICZ\hash_mica.json | ConvertFrom-Json
+$json=gc d:\SICZ\hash_mica.json | ConvertFrom-Json
 
 Write-Host -Object "$(Get-Date) - Dokonce nacitani json konfiguracniho souboru"
 
