5586df198ece7d8d1e81859c98402883c847608f
lukaskarabec <lukaskarabec@gmail.com>
2017-09-14 15:48:49 +0200

    14092017-1548

--- AVAS_LuKA/avas_LuKa.ps1
+++ AVAS_LuKA/avas_LuKa.ps1
@@ -1,7 +1,7 @@
 Start-Transcript -Path "./transcript$(get-date -f yyyy-MM-dd-hh-mm-ss).txt"
 #Nacteni JSON souboru s exportovanymi informacemi ze zkusebniho rozhrani
 Write-Host -Object "$(Get-Date) - Nacitani json konfiguracniho souboru"
-$json=gc d:\SICZ\hash_mica.json | ConvertFrom-Json
+$json = Get-Content d:\SICZ\hash_mica.json | ConvertFrom-Json
 
 Write-Host -Object "$(Get-Date) - Dokonce nacitani json konfiguracniho souboru"
 
@@ -10,7 +10,7 @@ Write-Host -Object "$(Get-Date) - zjisteni zda je uzivatel admin"
 #overeni ze je uzivatel administrator
 Write-Verbose "Kontroluji admin prava"
 If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
-    [Security.Principal.WindowsBuiltInRole] "Administrator")) {
+            [Security.Principal.WindowsBuiltInRole] "Administrator")) {
     Write-Warning "Ujistete se ze spoustite AVAS jako administrator! Provedte spusteni jako administrator nebo nemusi vse fungovat korektne!!"
 }
 
