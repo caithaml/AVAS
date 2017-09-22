abdb3b7e485fe2d41511f6cdcb5eaea18a24606b
lukaskarabec <lukaskarabec@gmail.com>
2017-09-22 12:20:33 +0200

    22092017-1220

---
+++ openfile.ps1
@@ -0,0 +1,13 @@
+
+Function Get-FileName($initialDirectory)
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+    
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $initialDirectory
+    $OpenFileDialog.filter = "CSV (*.csv)| *.csv"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    $OpenFileDialog.filename
+}
+$inputfile = Get-FileName "C:\SICZ"
+$inputdata = get-content $inputfile
