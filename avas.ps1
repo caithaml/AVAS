c6bb77d64a922de55981d2e998f89a171d604fcd
lukaskarabec <lukaskarabec@gmail.com>
2017-10-06 16:01:23 +0200

    06102017-1601

--- avas.ps1
+++ avas.ps1
@@ -259,3 +259,43 @@ Get-Content -Path "$scriptpath\config.ini"  | ForEach-Object -Begin {
          Write-Host -message "pokracuji ve zpracovani bez testu applikaci"
           }
   
+             
+        if($set.tasksdifftest -eq '1')
+        {
+          Write-Host "spoustim test tasks diff"
+          function servicediff
+          {
+            $app = Get-Content -Path D:\SICZ\app_default.json | ConvertFrom-Json  
+            $app2 = Get-Content -Path D:\SICZ\app.json | ConvertFrom-Json  
+          
+            $Diff = ForEach ($line1 in $app)   
+            {
+              ForEach ($line2 in $app2)   
+              {
+                IF ($line1.DisplayName -eq $line2.DisplayName)   # If stejny nazev
+                {
+                  IF ($line1.DisplayVersion -ne $line2.DisplayVersion)   # If jina verze
+                  {        
+                    New-Object -TypeName PSObject -Property @{
+                      DisplayName    = $line1.DisplayName
+                      DisplayVersion = $line1.DisplayVersion
+                      Publisher      = $line1.Publisher
+                    }  
+                  }
+                }
+              }                                                
+            }
+              
+            $Diff | Select-Object -Property DisplayName, DisplayVersion, Publisher 
+          
+          }
+    servicediff
+    $apps = servicediff
+    
+            }      
+          
+          else
+          {
+         Write-Host -message "pokracuji ve zpracovani bez testu naplanovanych uloh"
+          }
+  
