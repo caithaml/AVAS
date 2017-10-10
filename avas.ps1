70b3c0796e6800ac877d49fda875b6d68da55517
lukaskarabec <lukaskarabec@gmail.com>
2017-10-10 13:47:47 +0200

    úpravy cest, rozšíření informací

--- avas.ps1
+++ avas.ps1
@@ -30,7 +30,7 @@ Function Get-FileName($initialDirectory)
   $null = $OpenFileDialog.ShowDialog()
   $OpenFileDialog.filename
 }
-$inputfile                                   = Get-FileName 'D:\SICZ'
+$inputfile                                   = Get-FileName -initialDirectory 'D:\SICZ'
 $inputdata                                   = Get-Content $inputfile
 
 
@@ -69,119 +69,116 @@ Get-Content -Path "$scriptpath\config.ini"  | ForEach-Object -Begin {
   }
 }
 
-    if($set.debug -eq '1')
-  {
-   Start-Transcript -Path "$scriptpath/debug-$(Get-Date -Format yyyy-MM-dd-hh-mm-ss).log"
-    }
+if($set.debug -eq '1')
+{
+  Start-Transcript -Path "$scriptpath/debug-$(Get-Date -Format yyyy-MM-dd-hh-mm-ss).log"
+}
     
-    else
-    {
-   Write-Host -message "debug log neni aktivni, pokracuji dal"
-    }
+else
+{
+  Write-Host -message 'debug log neni aktivni, pokracuji dal'
+}
    
 
-    if($set.admin -eq '1')
-    {
-        Write-Verbose -Message "$(Get-Date) - zjisteni zda je uzivatel admin"
-        #overeni ze je uzivatel administrator
-        Write-Warning -Message 'Kontroluji admin prava'
-        if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
-        [Security.Principal.WindowsBuiltInRole] 'Administrator'))
-        {
-          Write-Warning -Message "Skript je nutne spustit s opravneni Administrator!  `n"
-          Break 
-        }      }
+if($set.admin -eq '1')
+{
+  Write-Verbose -Message "$(Get-Date) - zjisteni zda je uzivatel admin"
+  #overeni ze je uzivatel administrator
+  Write-Warning -Message 'Kontroluji admin prava'
+  if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+  [Security.Principal.WindowsBuiltInRole] 'Administrator'))
+  {
+    Write-Warning -Message "Skript je nutne spustit s opravneni Administrator!  `n"
+    Break 
+  }
+}
       
-      else
-      {
-     Write-Host -message "pokracuji ve zpracovani"
-      }
+else
+{
+  Write-Host -message 'pokracuji ve zpracovani'
+}
      
 
 
-      if($set.appdifftest -eq '1')
-      {
-        Write-Host "spoustim test app diff"
-        function appdiff
-        {
-          <#$def = Get-Content -Path $scriptpath\hash_mica.json | ConvertFrom-Json  
-          $new = Get-Content -Path $scriptpath\mica.json | ConvertFrom-Json#>  
+if($set.appdifftest -eq '1')
+{
+  Write-Host -Object 'spoustim test app diff'
+  function appdiff
+  {
+    <#$def = Get-Content -Path $scriptpath\hash_mica.json | ConvertFrom-Json  
+    $new = Get-Content -Path $scriptpath\mica.json | ConvertFrom-Json#>  
           
           
 
-          $app=$jsondef.installed_apps
-          $app2=$json.installed_apps
+    $app = $jsondef.installed_apps
+    $app2 = $json.installed_apps
         
-          $Diff = ForEach ($line1 in $app)   
-          {
-            ForEach ($line2 in $app2)   
-            {
-              IF ($line1.DisplayName -eq $line2.DisplayName)   # If stejny nazev
-              {
-                IF ($line1.DisplayVersion -ne $line2.DisplayVersion)   # If jina verze
-                {        
-                  New-Object -TypeName PSObject -Property @{
-                    DisplayName    = $line1.DisplayName
-                    DisplayVersion = $line1.DisplayVersion
-                    Publisher      = $line1.Publisher
-                  }  
-                }
-              }
-            }                                                
+    $Diff = ForEach ($line1 in $app)   
+    {
+      ForEach ($line2 in $app2)   
+      {
+        IF ($line1.DisplayName -eq $line2.DisplayName)   # If stejny nazev
+        {
+          IF ($line1.DisplayVersion -ne $line2.DisplayVersion)   # If jina verze
+          {        
+            New-Object -TypeName PSObject -Property @{
+              DisplayName    = $line1.DisplayName
+              DisplayVersion = $line1.DisplayVersion
+              Publisher      = $line1.Publisher
+            }  
           }
-            
-          $Diff | Select-Object -Property DisplayName, DisplayVersion, Publisher 
-        
         }
+      }                                                
+    }
+            
+    $Diff | Select-Object -Property DisplayName, DisplayVersion, Publisher
+  }
   appdiff
   $apps = appdiff
-  
-          }      
+}      
         
-        else
-        {
-       Write-Host -message "pokracuji ve zpracovani bez testu applikaci"
-        }
-        $hash | Add-Member Noteproperty Apps (appdiff)
+else
+{
+  Write-Host -message 'pokracuji ve zpracovani bez testu applikaci'
+}
+$hash | Add-Member Noteproperty Apps (appdiff)
 
 
     
     
-      if($set.servdiff -eq '1')
+if($set.servdiff -eq '1')
+{
+  Write-Host -Object 'spoustim test services diff'
+}
+function servdiff
+{
+  <#$def = Get-Content -Path $scriptpath\hash_luka_admin.json | ConvertFrom-Json  
+  $new = Get-Content -Path $scriptpath\mica.json | ConvertFrom-Json#>  
+
+  $app = $jsondef.services
+  $app2 = $json.services
+        
+  $Diff = ForEach ($line1 in $app)   
+  {
+    ForEach ($line2 in $app2)   
+    {
+      IF ($line1.DisplayName -eq $line2.DisplayName)   # If stejny nazev
       {
-        Write-Host "spoustim test services diff"
-      }
-        function servdiff
-        {
-          <#$def = Get-Content -Path $scriptpath\hash_luka_admin.json | ConvertFrom-Json  
-          $new = Get-Content -Path $scriptpath\mica.json | ConvertFrom-Json#>  
-
-          $app=$jsondef.services
-          $app2=$json.services
-        
-          $Diff = ForEach ($line1 in $app)   
-          {
-            ForEach ($line2 in $app2)   
-            {
-              IF ($line1.DisplayName -eq $line2.DisplayName)   # If stejny nazev
-              {
-                IF ($line1.StartType -ne $line2.StartType)   # If jina verze
-                {        
-                  New-Object -TypeName PSObject -Property @{
-                    DisplayName    = $line1.DisplayName
-                    StartType = $line1.StartType
-                    
-                  }  
-                }
-              }
-            }                                                
-          }
-            
-          $Diff | Select-Object -Property DisplayName, StartType
-        
+        IF ($line1.StartType -ne $line2.StartType)   # If jina verze
+        {        
+          New-Object -TypeName PSObject -Property @{
+            DisplayName = $line1.DisplayName
+            StartType   = $line1.StartType
+          }  
         }
-  servdiff  
-   $hash | Add-Member Noteproperty services (servdiff)
+      }
+    }                                                
+  }
+            
+  $Diff | Select-Object -Property DisplayName, StartType
+}
+servdiff  
+$hash | Add-Member Noteproperty services (servdiff)
       
 
 
@@ -189,176 +186,170 @@ Get-Content -Path "$scriptpath\config.ini"  | ForEach-Object -Begin {
       
       
            
-     if($set.schedulediff -eq '1')
+if($set.schedulediff -eq '1')
+{
+  Write-Host -Object 'spoustim test scheduled tasks diff'
+}
+function schedulediff
+{
+  <#$def = Get-Content -Path $scriptpath\hash_luka_admin.json | ConvertFrom-Json  
+  $new = Get-Content -Path $scriptpath\mica.json | ConvertFrom-Json#>  
+
+  $app = $jsondef.scheduled_tasks
+  $app2 = $json.scheduled_tasks
+        
+  $Diff = ForEach ($line1 in $app)   
+  {
+    ForEach ($line2 in $app2)   
+    {
+      IF ($line1.FormatEntryinfo -eq $line2.FormatEntryInfo)   # If stejny nazev
       {
-        Write-Host "spoustim test scheduled tasks diff"
-      }
-        function schedulediff
-        {
-          <#$def = Get-Content -Path $scriptpath\hash_luka_admin.json | ConvertFrom-Json  
-          $new = Get-Content -Path $scriptpath\mica.json | ConvertFrom-Json#>  
-
-          $app=$jsondef.scheduled_tasks
-          $app2=$json.scheduled_tasks
-        
-          $Diff = ForEach ($line1 in $app)   
-          {
-            ForEach ($line2 in $app2)   
-            {
-              IF ($line1.FormatEntryinfo -eq $line2.FormatEntryInfo)   # If stejny nazev
-              {
-                IF ($line1.Outofband -ne $line2.Outofband)   # If jina verze
-                {        
-                  New-Object -TypeName PSObject -Property @{
-                    formatentryinfo    = $line1.formatentryinfo
-                    outofband = $line1.outofband
-                    #Publisher      = $line1.Publisher
-                  }  
-                }
-              }
-            }                                                
-          }
-            
-          $Diff | Select-Object -Property formatentryinfo, outofband
-        
+        IF ($line1.Outofband -ne $line2.Outofband)   # If jina verze
+        {        
+          New-Object -TypeName PSObject -Property @{
+            formatentryinfo = $line1.formatentryinfo
+            outofband       = $line1.outofband
+          }  
         }
-  schedulediff  
-   $hash | Add-Member Noteproperty scheduledtasks (schedulediff)    
+      }
+    }                                                
+  }
+            
+  $Diff | Select-Object -Property formatentryinfo, outofband
+}
+schedulediff  
+$hash | Add-Member Noteproperty scheduledtasks (schedulediff)    
 
 
 
-    if($set.hotfixdiff -eq '1')
+if($set.hotfixdiff -eq '1')
+{
+  Write-Host -Object 'spoustim test hotfix diff'
+}
+function hotfixdiff
+{
+  <#$def = Get-Content -Path $scriptpath\hash_mica.json | ConvertFrom-Json  
+      $new = Get-Content -Path $scriptpath\hash_luka.json | ConvertFrom-Json  
+
+  #>          $app = $jsondef.hotfixes
+  $app2 = $json.hotfixes
+        
+  $Diff = ForEach ($line1 in $app)   
+  {
+    ForEach ($line2 in $app2)   
+    {
+      IF ($line1.HotfixID -eq $line2.HotfixID)   # If stejny nazev
       {
-        Write-Host "spoustim test hotfix diff"
-      }
-        function hotfixdiff
-        {
-          <#$def = Get-Content -Path $scriptpath\hash_mica.json | ConvertFrom-Json  
-          $new = Get-Content -Path $scriptpath\hash_luka.json | ConvertFrom-Json  
-
-  #>          $app=$jsondef.hotfixes
-          $app2=$json.hotfixes
-        
-          $Diff = ForEach ($line1 in $app)   
-          {
-            ForEach ($line2 in $app2)   
-            {
-              IF ($line1.HotfixID -eq $line2.HotfixID)   # If stejny nazev
-              {
-                IF ($line1.Description -ne $line2.Description)   # If jina verze
-                {        
-                  New-Object -TypeName PSObject -Property @{
-                    HotfixID    = $line1.HotfixID
-                    Description = $line1.Description
-                    #Publisher      = $line1.Publisher
-                  }  
-                }
-              }
-            }                                                
-          }
-            
-          $Diff | Select-Object -Property HotfixID, Description
-        
+        IF ($line1.Description -ne $line2.Description)   # If jina verze
+        {        
+          New-Object -TypeName PSObject -Property @{
+            HotfixID    = $line1.HotfixID
+            Description = $line1.Description
+          }  
         }
-  hotfixdiff  
-   $hash | Add-Member Noteproperty hotfix (hotfixdiff)
+      }
+    }                                                
+  }
+            
+  $Diff | Select-Object -Property HotfixID, Description
+}
+hotfixdiff  
+$hash | Add-Member Noteproperty hotfix (hotfixdiff)
    
 
 
-    <#if($set.processesdiff -eq '1')
-      {
-        Write-Host "spoustim test services diff"
-      }
-        function processesdiff
-        {
-          $def = Get-Content -Path $scriptpath\hash_luka.json | ConvertFrom-Json  
-          $new = Get-Content -Path $scriptpath\mica.json | ConvertFrom-Json  
+<#if($set.processesdiff -eq '1')
+    {
+    Write-Host "spoustim test services diff"
+    }
+    function processesdiff
+    {
+    $def = Get-Content -Path $scriptpath\hash_luka.json | ConvertFrom-Json  
+    $new = Get-Content -Path $scriptpath\mica.json | ConvertFrom-Json  
 
-          $app=$def.processes
-          $app2=$new.processes
+    $app=$def.processes
+    $app2=$new.processes
         
-          $Diff = ForEach ($line1 in $app)   
-          {
-            ForEach ($line2 in $app2)   
-            {
-              IF ($line1.Product -eq $line2.Product)   # If stejny nazev
-              {
-                IF ($line1.Description -ne $line2.Description)   # If jina verze
-                {        
-                  New-Object -TypeName PSObject -Property @{
-                    Product    = $line1.Product
-                    Description = $line1.Description
-                    #Publisher      = $line1.Publisher
-                  }  
-                }
-              }
-            }                                                
-          }
+    $Diff = ForEach ($line1 in $app)   
+    {
+    ForEach ($line2 in $app2)   
+    {
+    IF ($line1.Product -eq $line2.Product)   # If stejny nazev
+    {
+    IF ($line1.Description -ne $line2.Description)   # If jina verze
+    {        
+    New-Object -TypeName PSObject -Property @{
+    Product    = $line1.Product
+    Description = $line1.Description
+    #Publisher      = $line1.Publisher
+    }  
+    }
+    }
+    }                                                
+    }
             
-          $Diff | Select-Object -Property Product, Description
+    $Diff | Select-Object -Property Product, Description
         
-        }
+    }
 
     processesdiff  
     $hash | Add-Member Noteproperty processes (processesdiff)
 #>   
    
 
-   if($set.uwpdiff -eq '1')
-      {
-        Write-Host "spoustim test uwp apps diff"
-      }
-        function uwpdiff
-        {
-          <#$def = Get-Content -Path  $scriptpath\mica.json | ConvertFrom-Json  
-          $new = Get-Content -Path  $scriptpath\hash_mica.json | ConvertFrom-Json  
+if($set.uwpdiff -eq '1')
+{
+  Write-Host -Object 'spoustim test uwp apps diff'
+}
+function uwpdiff
+{
+  <#$def = Get-Content -Path  $scriptpath\mica.json | ConvertFrom-Json  
+      $new = Get-Content -Path  $scriptpath\hash_mica.json | ConvertFrom-Json  
   #>
-          $app=$jsondef.uwp_apps
-          $app2=$json.uwp_apps
+  $app = $jsondef.uwp_apps
+  $app2 = $json.uwp_apps
         
-          $Diff = ForEach ($line1 in $app)   
-          {
-            ForEach ($line2 in $app2)   
-            {
-              IF ($line1.Name-eq $line2.Name)   # If stejny nazev
-              {
-                IF ($line1.PackageFullName -ne $line2.PackageFullName)   # If jina verze
-                {        
-                  New-Object -TypeName PSObject -Property @{
-                    Name    = $line1.Name
-                   PackageFullName = $line1.PackageFullName
-                    #Publisher      = $line1.Publisher
-                  }  
-                }
-              }
-            }                                                
-          }
-            
-          $Diff | Select-Object -Property Name, PackageFullName
-        
+  $Diff = ForEach ($line1 in $app)   
+  {
+    ForEach ($line2 in $app2)   
+    {
+      IF ($line1.Name -eq $line2.Name)   # If stejny nazev
+      {
+        IF ($line1.PackageFullName -ne $line2.PackageFullName)   # If jina verze
+        {        
+          New-Object -TypeName PSObject -Property @{
+            Name            = $line1.Name
+            PackageFullName = $line1.PackageFullName
+          }  
         }
-  uwpdiff  
-   $hash | Add-Member Noteproperty uwp (uwpdiff)
+      }
+    }                                                
+  }
+            
+  $Diff | Select-Object -Property Name, PackageFullName
+}
+uwpdiff  
+$hash | Add-Member Noteproperty uwp (uwpdiff)
    
 
 
- $hash | Add-Member Noteproperty Active_DC ($json.Active_DC)
- $hash | Add-Member Noteproperty Allow_deviceclasses ($json.Allow_DeviceClasses)
- $hash | Add-Member Noteproperty Allow_deviceids ($json.Allow_DeviceIDs)
- $hash | Add-Member Noteproperty Application_log_length ($json.Application_Log_Length)
- $hash | Add-Member Noteproperty Applocker ($json.AppLocker)
- $json.BIOS
- $json.BIOS_Date
- $json.Bitlocker
- $json.Computer_Root_Certificates
- $json.ComputerName
- $json.Country
- $json.Date
- $json.Default_Locale
- $json.Deny_DeviceClasses
- $json.Deny_DeviceIDs
- $json.Deny_UnspecifiedDevices
- $json.Domain
- $json.Domain_DHCP
- $json.Domain_TCP
+$hash | Add-Member Noteproperty Active_DC ($json.Active_DC)
+$hash | Add-Member Noteproperty Allow_deviceclasses ($json.Allow_DeviceClasses)
+$hash | Add-Member Noteproperty Allow_deviceids ($json.Allow_DeviceIDs)
+$hash | Add-Member Noteproperty Application_log_length ($json.Application_Log_Length)
+$hash | Add-Member Noteproperty Applocker ($json.AppLocker)
+$hash | Add-Member Noteproperty BIOS ($json.BIOS)
+$hash | Add-Member Noteproperty Bios_dater ($json.BIOS_Date)
+$hash | Add-Member Noteproperty Bitlocker ($json.Bitlocker)
+$json.Computer_Root_Certificates
+$json.ComputerName
+$json.Country
+$json.Date
+$json.Default_Locale
+$json.Deny_DeviceClasses
+$json.Deny_DeviceIDs
+$json.Deny_UnspecifiedDevices
+$json.Domain
+$json.Domain_DHCP
+$json.Domain_TCP
  
