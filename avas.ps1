58c7c096a9bf95b78c16347ce3abc37368d085fd
lukaskarabec <lukaskarabec@gmail.com>
2017-10-13 10:41:37 +0200

    1041

--- avas.ps1
+++ avas.ps1
@@ -361,3 +361,54 @@ $hash.PCinfo
 $hash | Add-Member NoteProperty Local_groups ($json.Local_Groups)
 
 $hash
+
+function checktest
+{
+
+  $newLine = [System.Environment]::NewLine
+$Check=
+
+  
+  $outputBox.text = $Check
+}
+
+
+    Add-Type -AssemblyName System.Windows.Forms 
+    Add-Type -AssemblyName System.Drawing 
+    $MyForm = New-Object System.Windows.Forms.Form 
+    $MyForm.Text="oknoformulare" 
+  $MyForm.Size = New-Object System.Drawing.Size(1024,800) 
+   
+ 
+    $mTextBox1 = New-Object System.Windows.Forms.TextBox 
+        $mTextBox1.Text="TextBox1" 
+        $mTextBox1.Top="5" 
+        $mTextBox1.Left="5" 
+        $mTextBox1.Anchor="Left,Top" 
+    $mTextBox1.Size = New-Object System.Drawing.Size(100,23) 
+    $MyForm.Controls.Add($mTextBox1) 
+     
+ 
+    $mButton1 = New-Object System.Windows.Forms.Button 
+        $mButton1.Text="Button1" 
+        $mButton1.Top="13" 
+        $mButton1.Left="622" 
+        $mButton1.Anchor="Left,Top" 
+    $mButton1.Size = New-Object System.Drawing.Size(100,23) 
+    $mButton1.Add_Click({
+    checktest
+})
+    $MyForm.Controls.Add($mButton1) 
+   # $MyForm.ShowDialog()
+
+$outputBox = New-Object -TypeName System.Windows.Forms.TextBox
+$outputBox.Location = New-Object -TypeName System.Drawing.Size -ArgumentList (10, 200)
+$outputBox.Size = New-Object -TypeName System.Drawing.Size -ArgumentList (565, 200)
+$outputBox.MultiLine = $true
+$outputBox.ScrollBars = 'Vertical'
+$MyForm.Controls.Add($outputBox)
+
+$MyForm.Add_Shown({
+    $MyForm.Activate()
+})
+[void] $MyForm.ShowDialog()
